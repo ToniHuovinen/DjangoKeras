b7055d1a7d0de7cd71032c49eca2b0f407a8f972
Toni Huovinen <toni.huovinen@edu.turkuamk.fi>
2019-01-11 13:39:23 +0200

    Modified the model a bit, added charts.

--- keras_model/training.py
+++ keras_model/training.py
@@ -4,6 +4,7 @@ This will save it in h5 format to this folder. That file then will be loaded in 
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import keras
@@ -34,11 +35,13 @@ classifier = Sequential()
 
 # Add input layer and first hidden layer. Activation Function Relu and on output Sigmoid
 classifier.add(Dense(units=12, kernel_initializer= 'uniform', activation= 'relu', input_dim=8))
-classifier.add(Dropout(rate = 0.1))
 
 # Add second hidden layer. Activation Function Relu 
 classifier.add(Dense(units=8, kernel_initializer= 'uniform', activation= 'relu'))
-classifier.add(Dropout(rate = 0.1))
+classifier.add(Dropout(rate = 0.4))
+
+# Add third hidden layer. Activation Function Relu 
+classifier.add(Dense(units=6, kernel_initializer= 'uniform', activation= 'relu'))
 
 # Output Layer
 classifier.add(Dense(units=1, kernel_initializer= 'uniform', activation= 'sigmoid'))
@@ -47,7 +50,7 @@ classifier.add(Dense(units=1, kernel_initializer= 'uniform', activation= 'sigmoi
 classifier.compile(optimizer= 'adam', loss= 'binary_crossentropy', metrics=['accuracy'])
 
 # Fitting the ANN into the training set
-classifier.fit(X_train, y_train, batch_size=10, epochs=200)
+fitted_model = classifier.fit(X_train, y_train, batch_size=10, epochs=30)
 
 # Predictions
 y_pred = classifier.predict(X_test)
@@ -63,3 +66,33 @@ print(cm)
 
 classifier.save("diab.h5")
 print("Model saved")
+
+
+#############################
+# TOOLS AND SUCH
+
+# For checking out the max values of dataset
+# Min values are 0
+print("Max values in dataset:")
+print(dataset.max())
+
+# These are best run in Anaconda for example
+# PLOTTING
+# ACCURACY
+#plt.plot(fitted_model.history['acc'])
+#plt.plot(fitted_model.history['val_acc'])
+#plt.title('Model Accuracy')
+#plt.ylabel('accuracy')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
+
+# PLOTIING
+# MODEL LOSS
+#plt.plot(fitted_model.history['loss'])
+#plt.plot(fitted_model.history['val_loss'])
+#plt.title('Model Loss')
+#plt.ylabel('Loss')
+#plt.xlabel('Epoch')
+#plt.legend(['train','test'], loc='upper right')
+#plt.show()
