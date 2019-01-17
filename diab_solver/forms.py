29f23fbae676a7ee5d8ef9c746d253c8b1e329a5
Toni Huovinen <toni.huovinen@edu.turkuamk.fi>
2019-01-17 10:50:41 +0200

    Added value range for all the fields

--- diab_solver/forms.py
+++ diab_solver/forms.py
@@ -3,11 +3,11 @@ from django import forms
 
 class FormANN(forms.Form):
 
-	num_pregnant = forms.FloatField()
-	glucose_conc = forms.FloatField()
-	blood_pressure = forms.FloatField()
-	triceps = forms.FloatField()
-	insulin = forms.FloatField()
-	bmi = forms.FloatField()
-	pedigree = forms.FloatField()
-	age = forms.IntegerField()
+	num_pregnant = forms.FloatField(label="No. pregnacies (0 - 20)")
+	glucose_conc = forms.FloatField(label="Glucose (0 - 200)")
+	blood_pressure = forms.FloatField(label="Blood pressure (0 - 130)")
+	triceps = forms.FloatField(label="Skin thickness (0 - 100)")
+	insulin = forms.FloatField(label="Insulin (0 - 900)")
+	bmi = forms.FloatField(label="BMI (0 - 70)")
+	pedigree = forms.FloatField(label="Diabetes ped function (0 - 3)")
+	age = forms.IntegerField(label="Age (0 - 90)")
